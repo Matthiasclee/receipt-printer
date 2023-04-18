6eed212d24d7d7141e247cbca12d1e0d4ff02fa0
Matthias Lee <matthias@matthiasclee.com>
2023-04-17 19:56:26 -0700

    Don't make alert; wait for job to complete first

--- print.sh
+++ print.sh
@@ -6,7 +6,11 @@ done
 
 text="$text\n\n\n\n"
 
-echo -e "$text" | lp -d $1
-zenity --info --text="Press OK to cut paper" --no-wrap --title="Printer" --icon-name="edit-cut"
+echo -e "$text" | lp -o wait -d $1
+while lpstat -p $1 | grep -q "printing"; do
+  printf "\rWaiting for print job to complete"
+done
+echo "\nPrint job complete"
+echo "Sending signal to cut paper"
 echo -e "\x1B@\x1DV1" | nc -q 0 $2 9100
-
+echo "Done"
