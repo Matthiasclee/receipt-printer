1ce1adf498e7d5158338c744c2f28cc3e0d1fbed
Matthias Lee <matthias@matthiasclee.com>
2023-04-17 20:23:41 -0700

    -e

--- print.sh
+++ print.sh
@@ -10,7 +10,7 @@ echo -e "$text" | lp -o wait -d $1
 while lpstat -p $1 | grep -q "printing"; do
   printf "\rWaiting for print job to complete"
 done
-echo "\nPrint job complete"
+echo -e "\nPrint job complete"
 echo "Sending signal to cut paper"
 echo -e "\x1B@\x1DV1" | nc -q 0 $2 9100
 echo "Done"
